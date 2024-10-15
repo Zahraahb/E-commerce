58f68093d64cb2f9aba1f1223899e9a20a7cb906
Zahraahb <zahraahb55@gmail.com>
2024-10-15 04:26:28 +0300

    m

--- utils/pdf.js
+++ utils/pdf.js
@@ -1,7 +1,6 @@
 import fs from "fs";
 import PDFDocument from "pdfkit";
 
-import cloudinary from "./cloudinary";
 
 export async function createInvoice(invoice, path) {
   let doc = new PDFDocument({ size: "A4", margin: 50 });
